f04fee2011c4fb6e87f2e90f980884fc486b59c0
Alvin Neri <mcalvin.neri@fligno.com>
2021-11-30 09:10:36 +0800

    add push notif

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        React Native
+      </Text>
+    </View>
+  )
+}
+
+export default App;
